3cb57e9fd2baa2e424cefb5361750e6abd608897
rian3 <riantri271@gmail.com>
2019-01-06 23:06:52 +0700

    ini yang sa ubah beibh

--- page/wig/wigtambah.php
+++ page/wig/wigtambah.php
@@ -2,12 +2,12 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Objek Wisata</h1>
+                <h1 class="m-0 text-dark">WIG</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="?page=beranda">Beranda</a></li>
-                    <li class="breadcrumb-item active">Objek Wisata</li>
+                    <li class="breadcrumb-item active">WIG</li>
                 </ol>
             </div>
         </div>
